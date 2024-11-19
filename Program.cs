8ea2023a9f1a217038f28c9691d8cc618c88266d
AlekseyKolesnikov-nk <Aleksey.Kolesnikov@evraz.com>
2024-11-19 16:23:48 +0700

    К коду задания 14.2.10 добавлена сортировка по имени, затем по фамилии

--- Program.cs
+++ Program.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using static System.Net.Mime.MediaTypeNames;
-using System.Text;
-using System.Runtime.InteropServices;
-using Module14.Task;
-
-namespace Module14.Task;
+﻿namespace Module14.Task;
 
 class Program
 {
@@ -15,7 +7,6 @@ class Program
         //  создаём пустой список с типом данных Contact
         var phoneBook = new List<Contact>();
 
-        // добавляем контакты
         phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
         phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
         phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
@@ -23,53 +14,54 @@ class Program
         phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
         phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
 
-        // бесконечный цикл, ожидающий ввод с консоли
+        var sortedContacts = phoneBook
+            .OrderBy(sc => sc.Name)
+            .ThenBy(sc => sc.LastName);
+
+        Console.WriteLine("Для вывода на экран введите номер страницы телефонной книги 1, 2 или 3");
+
         while (true)
         {
-            var keyChar = Console.ReadKey().KeyChar; // получаем символ с консоли
-            Console.Clear();  //  очистка консоли от ввода
-
+            var keyChar = Console.ReadKey().KeyChar;
+            Console.Clear();
 
             if (!Char.IsDigit(keyChar))
             {
-                Console.WriteLine("Ошибка ввода, введите число");
+                Console.WriteLine("Ошибка ввода, введите 1, 2 или 3");
             }
             else
             {
-                //  переменная для хранения запроса в зависимости от введенного с консоли числа
                 IEnumerable<Contact> page = null;
 
-                //  выбираем нужное кол-во элементов для создания постраничного ввода в зависимости от запроса
                 switch (keyChar)
                 {
                     case ('1'):
-                        page = phoneBook.Take(2);
+                        page = sortedContacts.Take(2);
                         break;
                     case ('2'):
-                        page = phoneBook.Skip(2).Take(2);
+                        page = sortedContacts.Skip(2).Take(2);
                         break;
                     case ('3'):
-                        page = phoneBook.Skip(4).Take(2);
+                        page = sortedContacts.Skip(4).Take(2);
                         break;
                 }
 
-                //   проверим, что ввели существующий номер страницы
                 if (page == null)
                 {
-                    Console.WriteLine($"Ошибка ввода, страницы {keyChar} не существует");
+                    Console.WriteLine($"Ошибка ввода, страницы {keyChar} не существует, введите 1, 2 или 3");
                     continue;
                 }
 
-                // вывод результата на консоль
                 foreach (var contact in page)
                     Console.WriteLine(contact.Name + " " + contact.LastName + " " + contact.PhoneNumber + " " + contact.Email);
             }
         }
     }
 }
-public class Contact // модель класса
+
+public class Contact
 {
-    public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+    public Contact(string name, string lastName, long phoneNumber, String email)
     {
         Name = name;
         LastName = lastName;
